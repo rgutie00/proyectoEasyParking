1deeb6f301686ca987498a2f26f3a5aca47cc700
rgutie00 <rgutie00@gmail.com>
2022-08-29 18:47:34 -0500

    creacion proyecto

---
+++ src/main/java/com/easyparking/O2802/O2802Application.java
@@ -0,0 +1,13 @@
+package com.easyparking.O2802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2802Application.class, args);
+	}
+
+}
